3ce56595a440fceea5603673cde39095b5cf21b0
kapoloss <enesakif_kaplan@hotmail.com>
2025-01-30 20:08:18 +0300

    ThreeNeighboursStartegy Added

---
+++ Assets/Project1/Scripts/Core/ThreeNeighboursStrategy.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ThreeNeighboursStrategy : IWinStrategy
+{
+    private readonly Vector2Int[] _neighbours = new Vector2Int[]
+    {
+        new Vector2Int(-1, 0), 
+        new Vector2Int(1, 0),
+        new Vector2Int(0, -1),
+        new Vector2Int(0, 1),
+    };
+    
+
+    public bool CanWin(GridObject lastMarkedGrid, GridSystem<GridObject> grid, out List<GridObject> winningGrids)
+    {
+        winningGrids = new List<GridObject>();
+        HashSet<GridObject> checkedGrids = new HashSet<GridObject>();
+        Queue<GridObject> queue = new Queue<GridObject>();
+
+        if (lastMarkedGrid == null || !lastMarkedGrid.GetValue()) return false;
+
+        queue.Enqueue(lastMarkedGrid);
+        checkedGrids.Add(lastMarkedGrid);
+
+        while (queue.Count > 0)
+        {
+            GridObject current = queue.Dequeue();
+            winningGrids.Add(current);
+
+            foreach (var direction in _neighbours)
+            {
+                if (grid.FindNeighbour(current, direction, out var neighbour) && neighbour.GetValue() && !checkedGrids.Contains(neighbour))
+                {
+                    queue.Enqueue(neighbour);
+                    checkedGrids.Add(neighbour);
+                }
+            }
+        }
+        
+        return winningGrids.Count >= 3;
+    }}
